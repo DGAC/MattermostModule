969e317fdbaf2c01f053e2774376e1b8e4117bb1
bruno.spyckerelle <bruno.spyckerelle@aviation-civile.gouv.fr>
2018-01-25 12:16:29 +0100

    Fixes

--- config/module.config.php
+++ config/module.config.php
@@ -38,13 +38,6 @@ return array(
         )
     ),
     'service_manager' => array(
-        'abstract_factories' => array(
-            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
-            'Zend\Log\LoggerAbstractServiceFactory'
-        ),
-        'aliases' => array(
-            'translator' => 'MvcTranslator'
-        ),
         'factories' => array(
             'mattermostservice' => 'MattermostMessenger\Factories\MattermostServiceFactory',
         )
